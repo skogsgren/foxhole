27f3bdbad6749c74ccc2bd8e88de397961acf863
Gustaf Gren <6107181+skogsgren@users.noreply.github.com>
2025-05-06 14:50:51 +0200

    add config (whoops)

---
+++ foxhole/config.py
@@ -0,0 +1,37 @@
+import os
+import sys
+from pathlib import Path
+
+MODELSPEC = "sentence-transformers/all-MiniLM-L6-v2"
+
+if sys.platform.startswith("linux"):
+    DATADIR = Path.home() / ".local" / "share" / "foxhole"
+    MANIFESTDIR = Path.home() / ".mozilla" / "native-messaging-hosts"
+
+elif sys.platform == "darwin":
+    DATADIR = Path.home() / "Library" / "Application Support" / "foxhole"
+    MANIFESTDIR = (
+        Path.home()
+        / "Library"
+        / "Application Support"
+        / "Mozilla"
+        / "NativeMessagingHosts"
+    )
+
+elif sys.platform == "win32":
+    localappdata = os.getenv("LOCALAPPDATA") or Path.home() / "AppData" / "Local"
+    DATADIR = Path(localappdata) / "foxhole"
+    MANIFESTDIR = (
+        Path(os.getenv("APPDATA") or Path.home() / "AppData" / "Roaming")
+        / "Mozilla"
+        / "NativeMessagingHosts"
+    )
+
+else:
+    raise RuntimeError(f"Unsupported platform: {sys.platform}")
+
+DATADIR = DATADIR.expanduser().resolve()
+MANIFESTDIR = MANIFESTDIR.expanduser().resolve()
+
+DOCPATH = (DATADIR / "doc.db").expanduser().resolve()
+VECPATH = (DATADIR / "vec.db").expanduser().resolve()
