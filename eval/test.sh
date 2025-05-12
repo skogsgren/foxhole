0517fb4752c45e77f6ccb33f71d15a2f8872b82e
Gustaf Gren <6107181+skogsgren@users.noreply.github.com>
2025-05-12 19:04:47 +0200

    adjust test script to include trec dataset conversion

--- eval/test.sh
+++ eval/test.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # MINIMAL BASH SCRIPT FOR TESTING
+set -e
 DATA=./00_data_dataset.db
 ANN=./00_data_ann.db
-rm "$DATA"
-rm "$ANN"
+TRECDIR=./00_data_trec.json
 
-python3 ./create_eval_dataset.py \
-    --query_file ./queries.txt \
-    --output_file "$DATA" \
-    -k 30
+rm -f "$DATA"
+rm -f "$ANN"
+rm -rf "$TRECDIR"
 
+python3 ./create_eval_dataset.py --query_file ./queries.txt --output_file "$DATA" -k 10
 python3 ./annotate.py -i "$DATA" -o "$ANN"
-sqlite3 "$ANN" "SELECT query,rank,id,url,label FROM annotations;"
+python3 ./create_trec_dataset.py -i "$ANN" -o "$TRECDIR"
