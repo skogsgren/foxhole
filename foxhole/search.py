844884cd29edffdb415027f64c0130f7ae33404f
Tobias Martens <64360791+arrobbe@users.noreply.github.com>
2025-05-15 21:33:21 +0200

    Update search.py
    updated chroma semantic search with chunking, significantly more complex

--- foxhole/search.py
+++ foxhole/search.py
@@ -63,8 +63,59 @@ class ChromaSemanticSearchEngine(SearchEngine):
     """Chroma Semantic Search Engine"""
     def __init__(self):
         import chromadb
-        cc = chromadb.Client()
-        self.collection = cc.get_or_create_collection(name="my_webpages")
+        #pip install git+https://github.com/brandonstarxel/chunking_evaluation.git
+        #this repo served also as source for the _chunker_to_collection() function
+        self.my_embedding_function = chromadb.utils.embedding_functions.SentenceTransformersEmbeddingFunction(
+                model_name="all-MiniLM-L6-v2"
+                )
+        #my_embedding_function = chromadb.utils.embedding_functions.OpenAIEmbeddingFunction(
+        #        api_key="OPENAI_API_KEY",
+        #        model_name="text-embedding-3-large"
+        #        )
+        self.collection_name = "my_webpages"
+        #cc = chromadb.Client()
+        #self.collection = cc.get_or_create_collection(name=self.collection_name)
+        from chunking_evaluation import ClusterSemanticChunker
+        self.my_chunker = ClusterSemanticChunker(self.my_embedding_function, max_chunk_size = 200)
+
+    def _chunker_to_collection(self, chunker, embedding_function, chroma_db_path:str = None, collection_name:str = None):
+        collection = None
+
+        if chroma_db_path is not None:
+            try:
+                chunk_client = chromadb.PersistentClient(path=chroma_db_path)
+                collection = chunk_client.create_collection(collection_name, embedding_function=embedding_function, metadata={"hnsw:search_ef":50})
+                print("Created collection: ", collection_name)
+            except Exception as e:
+                print("Failed to create collection: ", e)
+                pass
+                # This shouldn't throw but for whatever reason, if it does we will default to below.
+
+        collection_name = "auto_chunk"
+        if collection is None:
+            try:
+                self.chroma_client.delete_collection(collection_name)
+            except ValueError as e:
+                pass
+            collection = self.chroma_client.create_collection(collection_name, embedding_function=embedding_function, metadata={"hnsw:search_ef":50})
+
+        docs, metas = self._get_chunks_and_metadata(chunker)
+
+        BATCH_SIZE = 500
+        for i in range(0, len(docs), BATCH_SIZE):
+            batch_docs = docs[i:i+BATCH_SIZE]
+            batch_metas = metas[i:i+BATCH_SIZE]
+            batch_ids = [str(i) for i in range(i, i+len(batch_docs))]
+            collection.add(
+                documents=batch_docs,
+                metadatas=batch_metas,
+                ids=batch_ids
+            )
+
+            # print("Documents: ", batch_docs)
+            # print("Metadatas: ", batch_metas)
+
+        return collection
 
     def load_db(self, db_path:Path):
         # read database
@@ -75,9 +126,17 @@ class ChromaSemanticSearchEngine(SearchEngine):
             raise ValueError("No documents for ChromaSemanticSearch found.")
         urls, docs = zip(*res.fetchall())
         connection.close()
-        
+
         # add database documents to the collection
-        self.collection.upsert(docs, urls) # or add
+        #self.collection.upsert(docs, urls) # or add
+        try:
+            cc = chromadb.PersistentClient(path=db_path)
+            self.collection = cc.get_collection(name=self.collection_name, embedding_function=self.my_embedding_function)
+        except:
+            self.collection = self._chunker_to_collection(self.my_chunker, self.my_embedding_function, chroma_db_path=db_path, collection_name=self.collection_name)
+        if self.collection is None:
+            self.collection = self._chunker_to_collection(self.my_chunker, self.my_embedding_function)
+
 
     def search_db(self, query:str, top_k:int=5):
         # throw a warning for empty collections?
