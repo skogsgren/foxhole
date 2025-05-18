9090a25a9f278d2189fd72f6ca2d717ca8583851
Fredrik Boglind <fredrik.boglind@gmail.com>
2025-05-18 15:57:05 +0200

    added repr functions to search.py (in version containing BM25search)

--- foxhole/search.py
+++ foxhole/search.py
@@ -18,6 +18,9 @@ class SearchEngine(ABC):
         """Search the index for the query, return a tuple[indices, scores]"""
         pass
 
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__}>"
+
 
 class TFIDFSearchEngine(SearchEngine):
     def __init__(self) -> None:
@@ -40,33 +43,62 @@ class TFIDFSearchEngine(SearchEngine):
             raise ValueError("No documents found.")
 
         self.urls, self.docs = zip(*rows)
-        self.tfidf_matrix = self.vectorizer.fit_transform(self.docs)
+        self.tfidf_matrix = self.vectorizer.fit_transform(x.lower() for x in self.docs)
 
     def search_db(self, query: str, top_k: int = 5) -> tuple[list[int], list[float]]:
         """Search the index for the query, return list of URLs or IDs"""
         if self.tfidf_matrix is None:
             raise ValueError("TF-IDF matrix not initialized. Did you call load_db()?")
-        query_vector = self.vectorizer.transform([query])
+        query_vector = self.vectorizer.transform([query.lower()])
         similarities = cosine_similarity(query_vector, self.tfidf_matrix).flatten()
         top_indices = similarities.argsort()[::-1][:top_k]
         # we have to add one since sqlite indexes from 1
         return [i + 1 for i in top_indices], similarities[top_indices]
 
-
+    def __repr__(self) -> str:
+        return f"<TFIDFSearchEngine: {len(self.docs)} documents indexed>"
+    
 class BM25SearchEngine(SearchEngine):
     """BM25 Search Engine"""
+    def __init__(self):
+        #pip install rank_bm25
+        from rank_bm25 import BM25Plus #BM25 BM250kapi BM25L BM25Plus
+        self.urls = []
 
-    # TODO: Implement
+    def load_db(self, db_path: Path):
+        # read database
+        connection = sqlite3.connect(db_path)
+        cursor = connection.cursor()
+        res = cursor.execute("SELECT url, text FROM pages")
+        if res.fetchone() is None:
+            raise ValueError("No documents for ChromaSemanticSearch found.")
+        self.urls, docs = zip(*res.fetchall())
+        connection.close()
 
+        tokenized_docs = [re.findall(r"[\w']+", doc.strip()) for doc in docs]
+        self.bm25 = BM25Plus(tokenized_docs)
+
+    def search_db(self, query:str, top_k:int=5):
+        if self.urls == []:
+            raise ValueError("No corpus documents found. Did you call load_db()?")
+        # tokenize the query and return top urls
+        tokenized_query = re.findall(r"[\w']+", query.strip())
+        results = self.bm25.get_top_n(tokenized_query, self.urls, n=top_k)
+        return results
+
+    def __repr__(self) -> str:
+        return f"<BM25SearchEngine: {len(self.urls)} documents indexed>"
 
 class ChromaSemanticSearchEngine(SearchEngine):
     """Chroma Semantic Search Engine"""
+
     def __init__(self):
         import chromadb
+
         cc = chromadb.Client()
         self.collection = cc.get_or_create_collection(name="my_webpages")
 
-    def load_db(self, db_path:Path):
+    def load_db(self, db_path: Path):
         # read database
         connection = sqlite3.connect(db_path)
         cursor = connection.cursor()
@@ -75,12 +107,15 @@ class ChromaSemanticSearchEngine(SearchEngine):
             raise ValueError("No documents for ChromaSemanticSearch found.")
         urls, docs = zip(*res.fetchall())
         connection.close()
-        
-        # add database documents to the collection
-        self.collection.upsert(docs, urls) # or add
 
-    def search_db(self, query:str, top_k:int=5):
+        # add database documents to the collection
+        self.collection.upsert(docs, urls)  # or add
+
+    def search_db(self, query: str, top_k: int = 5):
         # throw a warning for empty collections?
         results = self.collection.query(query_texts=[query], n_results=top_k)
 
         return results
+    
+    def __repr__(self) -> str:
+        return f"<ChromaSemanticSearchEngine: {self.collection.count()} documents indexed>"
