1303d3c1c79440325b694b3e89812930d23df441
Gustaf Gren <6107181+skogsgren@users.noreply.github.com>
2025-05-21 08:25:10 +0200

    feat: basic cohen kappa IA score metric

--- foxhole/eval/metrics.py
+++ foxhole/eval/metrics.py
@@ -1,2 +1,19 @@
-def interannotator_agreement(llm: dict, man: dict):
-    pass
+import sqlite3
+from pathlib import Path
+
+from sklearn.metrics import cohen_kappa_score
+
+
+def interannotator_agreement(man: Path, llm: Path) -> float:
+    def load_labels(sqlite_path):
+        conn = sqlite3.connect(sqlite_path)
+        cursor = conn.cursor()
+        cursor.execute("SELECT label FROM annotations")
+        labels = [row[0] for row in cursor.fetchall()]
+        conn.close()
+        return labels
+
+    man_labels = load_labels(man)
+    llm_labels = load_labels(llm)
+    assert len(llm_labels) == len(man_labels)
+    return cohen_kappa_score(man_labels, llm_labels)
