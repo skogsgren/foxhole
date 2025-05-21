f2a7f302774a3a52bc666228d9f7cd8b49838afd
Fredrik Boglind <fredrik.boglind@gmail.com>
2025-05-21 06:17:51 +0200

    evaluation.py and associated test file runs

--- tests/test_evaluation.py
+++ tests/test_evaluation.py
@@ -8,26 +8,10 @@ ANNOTATION_POOL_PATH = Path("annotation_pool.json")
 XRELS_PATH = Path("xrels.json")
 
 if __name__ == "__main__":
-    # from ir_measures import nDCG, MAP
-    # from ir_measures import evaluate
-
-    # eval = Evaluator(Path("xrels.json"))
-
-    # tfidf_run = {
-    #     "machine learning": [(42, 0.9), (105, 0.75)],
-    #     "list comprehension": [(200, 0.8), (88, 0.5)],
-    # }
-
-    # eval.add_run("tfidf", tfidf_run)
-
-    # results = evaluate(eval.qrels, eval.runs["tfidf"], [nDCG@10, MAP])
-    # print(results)
-
-
-
+    # Load the qrels and annotation pool
     evaluator = Evaluation(Path(XRELS_PATH), Path(ANNOTATION_POOL_PATH))
     results = evaluator.evaluate([nDCG@10, MAP])
-
+    # Print the results
     for system, metrics in results.items():
         print(f"System: {system}")
         for metric, value in metrics.items():
