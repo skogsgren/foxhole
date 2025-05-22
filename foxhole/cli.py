124b5299de13915df5f13a6d1b8b3912ec874bba
Gustaf Gren <6107181+skogsgren@users.noreply.github.com>
2025-05-22 14:44:48 +0200

    style: reformat according to ruff

--- foxhole/cli.py
+++ foxhole/cli.py
@@ -1,26 +1,30 @@
-import search
 import sys
 from pathlib import Path
 
+from . import search
 
 
 def check_method(search_method:str, search_methods:list):
+def check_method(search_method: str, search_methods: list):
     if search_method.isdigit():
-        search_method = search_methods[int(search_method)-1]
+        search_method = search_methods[int(search_method) - 1]
     method_provided = True
     if search_method not in search_methods:
         raise TypeError("ERROR: Search method must be out of", search_methods)
     return search_method
 
-def select_model(i:int, doc_path, vec_path):
-    if i == 1: se = search.TFIDFSearchEngine(doc_path, vec_path)
-    if i == 2: se = search.BM25SearchEngine(doc_path, vec_path)
-    if i == 3: se = search.ChromaSemanticSearchEngine(doc_path, vec_path)
+
+def select_model(i: int, doc_path, vec_path):
+    if i == 1:
+        se = search.TFIDFSearchEngine(doc_path, vec_path)
+    if i == 2:
+        se = search.BM25SearchEngine(doc_path, vec_path)
+    if i == 3:
+        se = search.ChromaSemanticSearchEngine(doc_path, vec_path)
     return se
 
 
 def main():
-
     # get db file name from command line
     try:
         db_path = Path(sys.argv[1])
@@ -40,14 +44,14 @@ def main():
     except:
         print("Warning: No 2nd path (to vec) provided!")
         vec_path = None
-        v = False #vector file provided?
+        v = False  # vector file provided?
     print("Vector file", vec_path, "selected.")
 
     # try getting optional top_k
-    k = False #top_k provided?
+    k = False  # top_k provided?
     try:
-        if sys.argv[2+int(v)][:3] == "top":
-            top_k = int(sys.argv[2+int(v)][3:])
+        if sys.argv[2 + int(v)][:3] == "top":
+            top_k = int(sys.argv[2 + int(v)][3:])
             k = True
             print("top_k =", top_k)
     except:
@@ -58,24 +62,23 @@ def main():
     # select search method to be used
     search_methods = ("TFIDF", "BM25", "ChromaSemantic")
     try:
-        search_method = check_method(sys.argv[2+int(k)+int(v)], search_methods)
+        search_method = check_method(sys.argv[2 + int(k) + int(v)], search_methods)
         method_provided = True
         print("Search method", search_method, "selected.")
     except:
-        #raise TypeError("ERROR: No search method provided!")
+        # raise TypeError("ERROR: No search method provided!")
         method_provided = False
         interactive = True
         print("No search method selected.")
 
     if method_provided:
-        se = select_model(search_methods.index(search_method)+1, db_path, vec_path)
+        se = select_model(search_methods.index(search_method) + 1, db_path, vec_path)
         se.load_db()
-
 
     # try getting query from command line or start interactive mode
     try:
-        _ = sys.argv[3+int(k)+int(v)]
-        query = " ".join(sys.argv[4+int(k)+int(v):])
+        _ = sys.argv[3 + int(k) + int(v)]
+        query = " ".join(sys.argv[4 + int(k) + int(v) :])
     except:
         interactive = True
 
@@ -87,18 +90,21 @@ def main():
         else:
             result = se.search_db(query)
         print("result:\n", result)
-    else: # interactive input loop
+    else:  # interactive input loop
         while True:
             if not method_provided:
                 user_input = input("Select search method or enter 'q' to quit:\n")
                 if user_input == "q":
                     break
-                try: search_method = check_method(user_input, search_methods)
+                try:
+                    search_method = check_method(user_input, search_methods)
                 except Exception as e:
                     print(e)
                     continue
                 print("Search method", search_method, "selected.")
-                se = select_model(search_methods.index(search_method)+1, db_path, vec_path)
+                se = select_model(
+                    search_methods.index(search_method) + 1, db_path, vec_path
+                )
                 se.load_db()
 
             user_input = input("Type whatever query or enter 'q' to quit:\n")
@@ -111,7 +117,6 @@ def main():
             else:
                 result = se.search_db(query)
             print("result:\n", result)
-
 
 
 if __name__ == "__main__":
